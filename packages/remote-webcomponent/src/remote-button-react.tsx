fc493d7532b3306e7b0daa8a8f19241fb216427f
Eddy <eddysocarras777@gmail.com>
2026-04-22 23:42:55 -0400

    Components from Lit and ReactJs

--- packages/remote-webcomponent/src/remote-button-react.tsx
+++ packages/remote-webcomponent/src/remote-button-react.tsx
@@ -1,29 +1,30 @@
 type RemoteButtonReactProps = Readonly<{
-  label?: string;
-  color?: string;
-  onClick?: () => void;
+    label?: string;
+    color?: string;
+    onClick?: () => void;
 }>;
 
 export function RemoteButtonReact({
-  label = 'Remote Button ReactJs',
-  color = '#1976d2',
-  onClick,
+    label = 'Remote Button ReactJs',
+    color = '#1976d2',
+    onClick,
 }: RemoteButtonReactProps) {
-  return (
-    <button
-      type="button"
-      onClick={onClick}
-      style={{
-        background: color,
-        color: 'white',
-        padding: '8px 12px',
-        fontSize: 14,
-        borderRadius: 6,
-        border: 'none',
-        cursor: 'pointer',
-      }}
-    >
-      {label}
-    </button>
-  );
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            style={{
+                background: color,
+                color: 'white',
+                padding: '8px 12px',
+                fontSize: 14,
+                borderRadius: 6,
+                border: 'none',
+                cursor: 'pointer',
+            }}
+        >
+            {label}
+        </button>
+    );
+
 }
